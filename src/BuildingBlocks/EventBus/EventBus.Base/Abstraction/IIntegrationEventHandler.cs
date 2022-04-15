ca04322f72987534add0a399e4d74cc1bd081da8
oguzhanaydiin <oguuzaydinn@gmail.com>
2022-04-16 00:26:27 +0300

    Add project files.

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IIntegrationEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using EventBus.Base.Events;
+
+namespace EventBus.Base.Abstraction
+{
+    public interface IIntegrationEventHandler<TEntegrationEvent> : IntegrationEventHandler where TEntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TEntegrationEvent @event);
+    }
+
+    public interface IntegrationEventHandler
+    {
+    }
+}
