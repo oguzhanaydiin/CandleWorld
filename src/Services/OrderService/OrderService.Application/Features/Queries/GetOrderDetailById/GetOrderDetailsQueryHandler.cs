35873e5a5f1c08d84559a011265d23e4edf6dffa
oguzhanaydiin <oguuzaydinn@gmail.com>
2022-06-08 16:42:07 +0300

    OrderService Application Layer

---
+++ src/Services/OrderService/OrderService.Application/Features/Queries/GetOrderDetailById/GetOrderDetailsQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using OrderService.Application.Features.Queries.ViewModels;
+using OrderService.Application.Interfaces.Repositories;
+
+namespace OrderService.Application.Features.Queries.GetOrderDetailById;
+
+public class GetOrderDetailsQueryHandler : IRequestHandler<GetOrderDetailsQuery, OrderDetailViewModel>
+{
+    IOrderRepository orderRepository;
+    private readonly IMapper mapper;
+    public GetOrderDetailsQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+    {
+        this.orderRepository = orderRepository;
+        this.mapper = mapper;
+    }
+
+    public async Task<OrderDetailViewModel> Handle(GetOrderDetailsQuery request, CancellationToken cancellationToken)
+    {
+        var order = await orderRepository.GetByIdAsync(request.OrderId, i => i.OrderItems);
+
+        var result = mapper.Map<OrderDetailViewModel>(order);
+
+        return result;
+    }
+}
